cdfa62d235238454503eae6f95c6070c6a8e1fec
Fabricio Catae <fabricio.catae@microsoft.com>
2015-08-11 17:12:07 -0300

    included a bare result cache implementation

---
+++ WinDashboard/ResultCache.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinDashboard
+{
+    class ResultCache
+    {
+        public void LoadWebsites(string filename)
+        {
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                string site;
+
+                while((site=reader.ReadLine()) != null)
+                {
+                    // make sure it starts with HTTP/HTTPS
+                    if (!site.ToLower().Contains("://"))
+                    {
+                        site = "http://" + site;
+                    }
+                }
+            }
+        }
+    }
+}
